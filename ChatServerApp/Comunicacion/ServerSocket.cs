839dfc6fbaf2f22d9133fc61e6ff7703548e7a2c
Christian Angulo <christiananguloprogramador23@gmail.com>
2021-04-08 11:37:04 -0400

    Proyecto finalizado

---
+++ ChatServerApp/Comunicacion/ServerSocket.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp.Comunicacion
+{
+
+    public class ServerSocket
+    {
+        private int puerto;
+        private Socket servidor;
+        private Socket comCliente;
+        private StreamReader reader;
+        private StreamWriter writer;
+       public ServerSocket(int puerto)
+        {
+            this.puerto = puerto;
+        }
+        public bool Iniciar()
+        {
+            try
+            {
+                //crea un socket
+                this.servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //toma el control del puerto
+                this.servidor.Bind(new IPEndPoint(IPAddress.Any, this.puerto));
+                //definir cuantos clientes atendere
+                this.servidor.Listen(10);
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool ObtenerCliente()
+        {
+            try
+            {
+                this.comCliente = this.servidor.Accept();
+                Stream stream = new NetworkStream(this.comCliente);
+                this.writer = new StreamWriter(stream);
+                this.reader = new StreamReader(stream);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Escribir(string mensaje)
+        {
+            try
+            {
+                this.writer.WriteLine(mensaje);
+                this.writer.Flush();
+                return true;
+            }
+            catch (IOException ex)
+            {
+
+                return false;
+            }
+        }
+
+        public string Leer()
+        {
+            try
+            {
+                return this.reader.ReadLine().Trim();
+            }
+            catch (IOException ex)
+            {
+                return null;
+            }
+        }
+        public void CerrarConexion()
+        {
+            this.comCliente.Close();
+        }
+    }
+}
---
+++ ChatServerApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using ChatServerApp.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Int32.Parse(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en purto {0},puerto");
+            ServerSocket servidor = new ServerSocket(puerto);
+            if(servidor.Iniciar())
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Esperando Clientes....");
+                if(servidor.ObtenerCliente())
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("Conexion Establecida!");
+                    Console.WriteLine("S:Hola mundo Cliente!");
+                    servidor.Escribir("hola mundo cliente");
+                    string mensaje = servidor.Leer();
+                    Console.WriteLine("C:{0}", mensaje);
+                    servidor.CerrarConexion();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No es posible Iniciar servidor");
+                Console.ReadKey();
+            }
+        }
+    }
+}
